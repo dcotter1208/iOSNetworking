622d8b8f41fb241d7b7d98cee3b5a8e7c67c941f
Cotter <Cotter@DonovanDLabsMacBookPro.local>
2016-08-21 19:18:23 -0400

    NSURLSession complete

---
+++ iOSNetworking/AFNetworkingTVC.h
@@ -0,0 +1,13 @@
+//
+//  AFNetworkingTVC.h
+//  iOSNetworking
+//
+//  Created by Cotter on 8/21/16.
+//  Copyright © 2016 Cotter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFNetworkingTVC : UITableViewController
+
+@end
