e79c551e5f8ce7619f1395a518095b2760a94147
Cotter <Cotter@donovandlabsmacbookpro.local>
2016-08-21 16:10:43 -0400

    Initial Commit

---
+++ iOSNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSNetworking
+//
+//  Created by Cotter on 8/21/16.
+//  Copyright © 2016 Cotter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSNetworking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSNetworking
+//
+//  Created by Cotter on 8/21/16.
+//  Copyright © 2016 Cotter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
